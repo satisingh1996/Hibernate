8217cf3751c37a8c533a69a594ad098febf65d18
Satish Singh <8793singhsatish@gmail.com>
2024-01-30 21:02:59 +0530

    Project Added

---
+++ StudentCrud/src/main/java/org/example/controller/MyApp.java
@@ -0,0 +1,45 @@
+package org.example.controller;
+
+
+import org.example.model.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class MyApp {
+    public static void main(String args[])
+    {
+        Configuration cfg = new Configuration().configure("configuration.cfg.xml");
+        SessionFactory sessionFactory = cfg.buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+        Transaction transaction=session.beginTransaction();
+
+//        Student student=new Student();
+
+//        student.setId(3);
+//        student.setName("xyz");
+//        student.setSalary(50000);
+
+//        session.save(student);
+//        transaction.commit();
+
+//        session.beginTransaction().commit();
+//        System.out.println("Data added to database");
+
+        Student s1=session.get(Student.class,2);
+        System.out.println(s1);
+
+        s1.setName("satish");
+
+        session.update(s1);
+
+        transaction.commit();
+
+        
+
+
+
+    }
+}
---
+++ StudentCrud/src/main/java/org/example/model/Student.java
@@ -0,0 +1,40 @@
+package org.example.model;
+
+public class Student {
+    private String name;
+    private int id;
+    private int salary;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
